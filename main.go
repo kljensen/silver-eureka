00c8d17a5e0b511ef3781c7a43f6456d698ae210
Kyle L. Jensen <kljensen@gmail.com>
2024-10-21 19:24:42 +0000

    Refactor main.go to print 'hello world' multiple times based on user input. Closes Allow user to print hello world N times #2

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func printHelloWorld(count int) {
+	for i := 0; i < count; i++ {
+		fmt.Println("hello world")
+	}
+}
+
 func main() {
-    fmt.Println("hello world")
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide the number of times to print 'hello world'")
+		return
+	}
+
+	count, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid number:", os.Args[1])
+		return
+	}
+	printHelloWorld(count)
 }
